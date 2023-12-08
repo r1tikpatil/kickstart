52e6cefe81e19430b88682f6a6d160b4ef434ed5
r1tikpatil <ritikpatil566@gmail.com>
2023-12-08 15:54:55 +0530

    added routes

---
+++ pages/campaigns/show.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "../../components/Layout";
+
+const CampaignShow = () => {
+  return (
+    <Layout>
+      <h3>Campaign Show!!</h3>
+    </Layout>
+  );
+};
+
+export default CampaignShow;
