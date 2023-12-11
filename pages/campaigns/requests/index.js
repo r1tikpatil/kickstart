d331b7153b4293047357d319beb079406116cb5b
r1tikpatil <ritikpatil566@gmail.com>
2023-12-11 22:35:58 +0530

    fetching all requests

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -4,15 +4,32 @@ import { useRouter } from "next/router";
 
 import Layout from "../../../components/Layout";
 import { Link } from "../../../routes";
+import Campaign from "../../../ethereum/campaign";
 
 const RequestIndex = () => {
   const router = useRouter();
   const [address, setAddress] = useState("");
+  const [requests, setRequests] = useState([]);
+  const [requestsCount, setRequestsCount] = useState(0);
 
   useEffect(() => {
     const handleRouteChange = async () => {
       const currentPath = router.asPath;
       setAddress(currentPath.slice(11, 53));
+
+      const campaign = Campaign(currentPath.slice(11, 53));
+      const requestCount = await campaign.methods.getRequestsCount().call();
+
+      const requests = await Promise.all(
+        Array(parseInt(requestCount))
+          .fill()
+          .map((element, index) => {
+            return campaign.methods.requests(index).call();
+          })
+      );
+
+      setRequests(requests);
+      setRequestsCount(requestCount);
     };
     handleRouteChange();
   }, [router.asPath]);
